967fa346b770fec17344c1adda3756bfb1e8095d
noelsison2 <nysison@gmail.com>
2017-03-20 14:52:14 +0800

    Added test to check if splash elements are displayed

--- app/src/androidTest/java/com/example/noel/videolist/SplashActivityTest.java
+++ app/src/androidTest/java/com/example/noel/videolist/SplashActivityTest.java
@@ -12,10 +12,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.fail;
 
 /**
  * Created by Noel on 3/20/2017.
@@ -38,10 +42,20 @@ public class SplashActivityTest {
         3. Button with text "Start"
          */
 
+        final int textViewGreetingId = R.id.splash_tv_greeting;
+        final int editTextNameId = R.id.splash_et_name;
+        final int buttonStartId = R.id.splash_b_start;
+
+        onView(withId(textViewGreetingId)).check(matches(withText(R.string.splash_greeting)));
+        onView(withId(editTextNameId)).check(matches(withHint(R.string.splash_hint_name)));
+        onView(withId(buttonStartId)).check(matches(withText(R.string.splash_button_start)));
+
         /*
         When user clicks "Start" button while the input field for "Name" is empty (no text or just
         spaces), display a toast with the message "Please enter your name".
          */
+
+        fail("Finish the test");
 
         /*
         When user enters a non-empty name in the input field  and clicks "Start" button, save the
